0fabfcf7644e7bf49e648316f523adeab9cf28e5
nurfajarismail <62985196+nurfajarismail@users.noreply.github.com>
2021-02-26 19:10:20 +0700

    add gambar

--- lib/firstPage.dart
+++ lib/firstPage.dart
@@ -4,7 +4,41 @@ class TampilanAwal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset(
+                "assets/images/goat.png",
+                scale: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.only(right: 30, left: 30),
+              width: double.infinity,
+              // ignore: deprecated_member_use
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                color: Colors.brown,
+                onPressed: () {
+                  print("hello world");
+                },
+                child: Text(
+                  "Mulai",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
